4e7b17abdb6d85b3ffab41c67936e4c0769cf801
astefanczyk <astefanczyk@student.agh.edu.pl>
2022-06-21 01:13:53 +0200

    Add project files.

---
+++ initManipulatorParameters.m
@@ -0,0 +1,25 @@
+% Gravity
+g = -9.81;
+
+% Sample Time
+Ts = 0.025;  % change to 0.025
+
+% Simulation Time
+Tf = 10;
+
+% Initial body speeds in x,y
+vx_init = 0;
+vy_init = 0;
+
+% % Hip and Knee Joint properties
+% joint.stiffness = 0;
+% joint.damping = 8;
+% joint.limitStiffness = 500;
+% joint.limitDamping = 50;
+% joint.transitionWidth = 2 * d2r;
+% hip_eq_angle = 0;
+% knee_eq_angle = 0;
+
+%% Define limits on variables
+% vars for isDone 
+
---
+++ initialize.m
@@ -0,0 +1,67 @@
+close all
+clearvars
+clc
+
+initManipulatorParameters
+
+mdl = 'v2';
+open_system(mdl)
+
+numObs = 16;
+obsInfo = rlNumericSpec([numObs 1]);
+obsInfo.Name = 'observations';
+
+numAct = 5;
+actInfo = rlNumericSpec([numAct 1],'LowerLimit',-1,'UpperLimit', 1);
+actInfo.Name = 'torque';
+
+blk = [mdl, '/RL Agent'];
+env = rlSimulinkEnv(mdl,blk,obsInfo,actInfo);
+
+% env.ResetFcn = @quadrupedResetFcn; %% write reset func
+env.ResetFcn = @ResetFcn;
+
+createNetwork
+
+agentOptions = rlDDPGAgentOptions;
+agentOptions.SampleTime = Ts;
+agentOptions.DiscountFactor = 0.99;
+agentOptions.MiniBatchSize = 250;
+agentOptions.ExperienceBufferLength = 1e6;
+agentOptions.TargetSmoothFactor = 1e-3;
+agentOptions.NoiseOptions.MeanAttractionConstant = 0.15;
+agentOptions.NoiseOptions.StandardDeviation = 0.1;
+
+agent = rlDDPGAgent(actor,critic,agentOptions);
+
+maxEpisodes = 10000;
+maxSteps = floor(Tf/Ts);  
+trainOpts = rlTrainingOptions(...
+    'MaxEpisodes',maxEpisodes,...
+    'MaxStepsPerEpisode',maxSteps,...
+    'ScoreAveragingWindowLength',250,...
+    'Verbose',true,...
+    'Plots','training-progress',...
+    'StopTrainingCriteria','AverageReward',...
+    'StopTrainingValue',190,...                   
+    'SaveAgentCriteria','EpisodeReward',... 
+    'SaveAgentValue',200);     
+
+trainOpts.UseParallel = true;                    
+trainOpts.ParallelizationOptions.Mode = 'async';
+trainOpts.ParallelizationOptions.StepsUntilDataIsSent = 32;
+trainOpts.ParallelizationOptions.DataToSendFromWorkers = 'Experiences';
+
+doTraining = true;
+if doTraining    
+    % Train the agent.
+    trainingStats = train(agent,env,trainOpts);
+else
+    % Load a pretrained agent for the example.
+    load('ManipulatorAgent.mat','agent')
+end
+
+rng(0)
+
+simOptions = rlSimulationOptions('MaxSteps',maxSteps);
+experience = sim(env,agent,simOptions);
